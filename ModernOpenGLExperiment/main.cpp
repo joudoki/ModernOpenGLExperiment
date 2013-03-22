d0277b4b7704c1f3fa04ec285d0887854fccdccb
Ryan Lewellen <rlewelle@mines.edu>
2013-03-22 03:06:22 -0600

    Cleanup junk cube-related stuff

--- ModernOpenGLExperiment/main.cpp
+++ ModernOpenGLExperiment/main.cpp
@@ -68,8 +68,8 @@ void setupOpenGL() {
 
     // Enable 3D ops
     glEnable(GL_DEPTH_TEST);
-    //glEnable(GL_CULL_FACE);
-    //glCullFace(GL_FRONT);
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_FRONT);
 
     // Setup windowing transform
     glViewport(0, 0, 800, 600);
@@ -128,85 +128,6 @@ Program* MakeProgram(const string& vertexShaderSource, const string& fragmentSha
     return program;
 }
 
-Mesh* MakeCubeMesh(Program* program) {
-#define COORD(px,py,pz,nx,ny,nz,u,v) {glm::vec3(px,py,pz), glm::vec3(nx,ny,nz), glm::vec2(u,v)}
-
-    /*
-COORD(-1.0f, -1.0f, -1.0f, 0.0f, 0.0f), // 0
-COORD(-1.0f, -1.0f,  1.0f, 0.0f, 1.0f), // 1
-COORD(-1.0f,  1.0f, -1.0f, 1.0f, 0.0f), // 2
-COORD(-1.0f,  1.0f,  1.0f, 1.0f, 1.0f), // 3
-COORD( 1.0f, -1.0f, -1.0f, 0.0f, 0.0f), // 4
-COORD( 1.0f, -1.0f,  1.0f, 0.0f, 1.0f), // 5
-COORD( 1.0f,  1.0f, -1.0f, 1.0f, 0.0f), // 6
-COORD( 1.0f,  1.0f,  1.0f, 1.0f, 1.0f)  // 7
-    */
-
-    MD3Model::Vertex_t vertexData[] = {
-        // -X side, 0,2,1,3
-        COORD(-1.0f, -1.0f, -1.0f,    -1.0f,  0.0f,  0.0f,    0.0f, 0.0f), // 0  0
-        COORD(-1.0f,  1.0f, -1.0f,    -1.0f,  0.0f,  0.0f,    1.0f, 0.0f), // 2  1
-        COORD(-1.0f, -1.0f,  1.0f,    -1.0f,  0.0f,  0.0f,    0.0f, 1.0f), // 1  2
-        COORD(-1.0f,  1.0f,  1.0f,    -1.0f,  0.0f,  0.0f,    1.0f, 1.0f), // 3  3
-
-        // -Y side, 5,4,1,0
-        COORD( 1.0f, -1.0f,  1.0f,     0.0f, -1.0f,  0.0f,    0.0f, 0.0f), // 5  4
-        COORD( 1.0f, -1.0f, -1.0f,     0.0f, -1.0f,  0.0f,    1.0f, 0.0f), // 4  5 
-        COORD(-1.0f, -1.0f,  1.0f,     0.0f, -1.0f,  0.0f,    0.0f, 1.0f), // 1  6
-        COORD(-1.0f, -1.0f, -1.0f,     0.0f, -1.0f,  0.0f,    1.0f, 1.0f), // 0  7
-
-        // -Z side, 4,6,0,2
-        COORD( 1.0f, -1.0f, -1.0f,     0.0f,  0.0f, -1.0f,    0.0f, 0.0f), // 4  8
-        COORD( 1.0f,  1.0f, -1.0f,     0.0f,  0.0f, -1.0f,    1.0f, 0.0f), // 6  9
-        COORD(-1.0f, -1.0f, -1.0f,     0.0f,  0.0f, -1.0f,    0.0f, 1.0f), // 0 10
-        COORD(-1.0f,  1.0f, -1.0f,     0.0f,  0.0f, -1.0f,    1.0f, 1.0f), // 2 11
-
-        // +X side, 5,7,4,6
-        COORD( 1.0f, -1.0f,  1.0f,     1.0f,  0.0f,  0.0f,    0.0f, 0.0f), // 5 12
-        COORD( 1.0f,  1.0f,  1.0f,     1.0f,  0.0f,  0.0f,    1.0f, 0.0f), // 7 13
-        COORD( 1.0f, -1.0f, -1.0f,     1.0f,  0.0f,  0.0f,    0.0f, 1.0f), // 4 14
-        COORD( 1.0f,  1.0f, -1.0f,     1.0f,  0.0f,  0.0f,    1.0f, 1.0f), // 6 15
-
-        // +Y side, 2,3,6,7
-        COORD(-1.0f,  1.0f, -1.0f,     0.0f,  1.0f,  0.0f,    1.0f, 0.0f), // 2 16
-        COORD( 1.0f,  1.0f, -1.0f,     0.0f,  1.0f,  0.0f,    0.0f, 0.0f), // 6 17
-        COORD(-1.0f,  1.0f,  1.0f,     0.0f,  1.0f,  0.0f,    1.0f, 1.0f), // 3 18
-        COORD( 1.0f,  1.0f,  1.0f,     0.0f,  1.0f,  0.0f,    0.0f, 1.0f), // 7 19
-
-        // +Z side, 1,3,5,7
-        COORD(-1.0f, -1.0f,  1.0f,     0.0f,  0.0f,  1.0f,    0.0f, 0.0f), // 1 20
-        COORD(-1.0f,  1.0f,  1.0f,     0.0f,  0.0f,  1.0f,    1.0f, 0.0f), // 3 21
-        COORD( 1.0f, -1.0f,  1.0f,     0.0f,  0.0f,  1.0f,    0.0f, 1.0f), // 5 22
-        COORD( 1.0f,  1.0f,  1.0f,     0.0f,  0.0f,  1.0f,    1.0f, 1.0f)  // 7 23
-    };
-    GLuint vertexCount = 24;
-
-#undef COORD
-
-    GLubyte indexData[] = {
-         0,  1,  2,   1,  3,  2,
-         4,  5,  6,   5,  7,  6,
-         8,  9, 10,   9, 11, 10,
-        12, 13, 14,  13, 15, 14,
-        16, 17, 18,  17, 19, 18,
-        20, 21, 22,  21, 23, 22
-    };
-    GLuint triangleCount = 12;
-    GLuint indexCount = 3*triangleCount;
-
-    GLsizei stride = 8*sizeof(GLfloat);
-    VertexAttributeBinding_t vertFmt[] = {
-        {0, 3, GL_FLOAT, GL_FALSE, stride, BUFFER_OFFSET(0)},
-        {1, 3, GL_FLOAT, GL_FALSE, stride, BUFFER_OFFSET(3*sizeof(GLfloat))},
-        {2, 2, GL_FLOAT, GL_FALSE, stride, BUFFER_OFFSET(6*sizeof(GLfloat))}
-    };
-
-    Mesh* mesh = new Mesh(PrimitiveType::TrianglesPrimitive, vertFmt, 3);
-    mesh->SetVertexData(vertexCount, vertexCount*stride, vertexData);
-    mesh->SetIndexData(IndexType::UnsignedByteIndex, indexCount, indexCount*sizeof(GLushort), indexData);
-
-    return mesh;
-}
 Mesh* MakeAxisMesh(Program* program) {
     float data[] = {
         0.0f, 0.0f, 0.0f,   0.0f, 0.0f, 0.0f,
@@ -399,11 +320,13 @@ int main(int argc, char** argv) {
         glfwTerminate();
         return EXIT_FAILURE;
     }
+
     float cameraDistance = 2.0f * model->GetFrame(0)->radius;
     Mesh* ammoBox = LoadMD3Mesh(textureShader, model);
-    Mesh* axis    = MakeAxisMesh(flatShader);
 
     delete model;
+
+    Mesh* axis    = MakeAxisMesh(flatShader);
 
     // Setup trackball interface
     Trackball trackball(width, height, 1.0f, glm::mat4());
